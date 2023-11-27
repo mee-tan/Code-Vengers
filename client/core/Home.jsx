2682d29f502abc43914acee0e94c636f53e24139
Misha Mansoor <mishatnvr@gmail.com>
2023-11-27 01:32:15 -0500

    added files for navigation, home page and initial setup

---
+++ client/core/Home.jsx
@@ -0,0 +1,77 @@
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import codevengersimg from './../assets/images/codevengersimg.png';
+import { Link } from 'react-router-dom';
+   
+
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    marginTop: theme.spacing(5),
+    color: '#444444',
+  },
+  title: {
+    padding: theme.spacing(3, 2.5, 2),
+    color: theme.palette.openTitle,
+  },
+  media: {
+    minHeight: 300,
+    minWidth:300,
+  },
+  paragraph: {
+    fontFamily: 'Oswald, sans-serif',
+  },
+  h1: {
+    fontFamily: 'Oswald, sans-serif',
+    fontSize: '1.5rem',
+    textAlign: 'center',
+  },
+  footer: {
+    backgroundColor:'#696969',
+    fontFamily: 'Oswald, sans-serif',
+    padding: theme.spacing(2),
+    marginTop: 'auto',
+    textAlign: 'center',
+    color:'black',
+  },
+}));
+
+export default function Home(){ 
+const classes = useStyles()
+return (
+<Card className={classes.card}>
+  <Card class={classes.card}>
+<CardContent>
+  <Typography variant="body2"  className= {classes.h1} component="h1"> 
+    Welcome to the Code Vengers Online Marketplace
+  </Typography> 
+  <Typography variant= "bod1" className= {classes.paragraph}component="p">
+  <br/>
+    Build your tech career
+    With CodeVengers plans, you can achieve your school project or career goals. sign up today and join our team
+    we will do our very best to set you up for the real world.
+  </Typography>
+</CardContent>
+</Card>
+
+<Typography variant="h6" className={classes.title}></Typography>
+<CardMedia className={classes.media}
+image={codevengersimg} title="Code Vengers"/>
+
+<Card className={classes.card}>
+</Card>
+<footer  position="static" className={classes.footer}>
+  <Typography variant="body2" color="inherit">
+  Made with ❤️© 2023 CodeVengers
+  </Typography>
+  </footer>
+</Card> 
+)
+}
+
---
+++ client/core/Menu.jsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import { useNavigate, Link, useLocation } from 'react-router-dom';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Button,
+  useMediaQuery,
+  Hidden,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import auth from '../lib/auth-helper'
+import { makeStyles } from '@material-ui/core/styles';
+
+const isActive = (location, path) => {
+  return location.pathname === path ? { color: '#ff4081' } : { color: 'black' };
+};
+
+const styles = {
+  fontFamily: 'Oswald',
+  marginLeft: 'auto',
+  color:'black',
+};
+
+const fonts = {
+  fontFamily: 'Oswald, sans-serif',
+  color:'black',
+};
+export default function Menu() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const isSmallScreen = useMediaQuery('(max-width:600px)');
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setDrawerOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setDrawerOpen(false);
+  };
+
+  const drawerContent = (
+    <List>
+      <ListItem button onClick={() => navigate('/')}>
+        <ListItemText primary="Home" />
+      </ListItem>
+      <ListItem button onClick={() => navigate('/resources')}>
+        <ListItemText primary="Resources" />
+      </ListItem>
+      <ListItem button onClick={() => navigate('/users')}>
+        <ListItemText primary="Users" />
+      </ListItem>
+    </List>
+  );
+
+  return (
+    <AppBar position="static" style={{ backgroundColor: '#696969' }}>
+      <Toolbar>
+        {isSmallScreen ? (
+          <>
+            <IconButton color="inherit" onClick={handleDrawerOpen}>
+              <MenuIcon />
+            </IconButton>
+            <Drawer anchor="left" open={drawerOpen} onClose={handleDrawerClose}>
+              <div onClick={handleDrawerClose} onKeyDown={handleDrawerClose}>
+                {drawerContent}
+              </div>
+            </Drawer>
+          </>
+        ) : (
+          <>
+            <Typography variant="h6" color="inherit" style={{...fonts}}>
+              CODE VENGERS
+            </Typography>
+            <Hidden xsDown>
+              <Link to="/">
+                <IconButton aria-label="Home" style={isActive(location, '/')}>
+                </IconButton>
+              </Link>
+              <Link to="/resources">
+                <Button style={{ ...isActive('/resources'), ...fonts }}>Resources</Button>
+              </Link>
+              <Link to="/users">
+                <Button style={{ ...isActive('/users'), ...fonts }}>Users</Button>
+              </Link>
+            </Hidden>
+          </>
+        )}
+        {!auth.isAuthenticated() && (
+          <Hidden xsDown>
+            <span>
+              <Link to="/signin">
+                <Button style={{ ...isActive('/signin'), ...fonts }}>Log In</Button>
+              </Link>
+            <Link to="/signup">
+              <Button style={{ ...isActive('/signup'), ...styles }}>Sign up</Button>
+            </Link>
+            </span>
+          </Hidden>
+        )}
+        {auth.isAuthenticated() && (
+          <Hidden xsDown>
+            <span>
+              <Link to={`/user/${auth.isAuthenticated().user._id}`}>
+                <Button style={isActive(location, `/user/${auth.isAuthenticated().user._id}`)}>
+                  My Profile
+                </Button>
+              </Link>
+              <Button
+                color="inherit"
+                onClick={() => {
+                  auth.clearJWT(() => navigate('/'));
+                }}
+              >
+                Sign out
+              </Button>
+            </span>
+          </Hidden>
+        )}
+      </Toolbar>
+    </AppBar>
+    );
+};
