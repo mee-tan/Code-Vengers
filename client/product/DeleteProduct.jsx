c7b5af41bc9ccbd5083863c04cef06df3db0aee3
GregKom <gregkomarn@gmail.com>
2023-12-08 12:04:23 -0500

    Some Troubleshoot fixes

--- client/product/DeleteProduct.jsx
+++ client/product/DeleteProduct.jsx
@@ -8,7 +8,7 @@ import DialogActions from '@material-ui/core/DialogActions'
 import DialogContent from '@material-ui/core/DialogContent'
 import DialogContentText from '@material-ui/core/DialogContentText'
 import DialogTitle from '@material-ui/core/DialogTitle'
-import auth from './../auth/auth-helper'
+import auth from './../lib/auth-helper'
 import {remove} from './api-product.js'
 
 export default function DeleteProduct(props) {
