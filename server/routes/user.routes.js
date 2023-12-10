1024848c1488870319e89f67e50714846b200c70
Lucianna Mendonca <luciannamend@gmail.com>
2023-12-09 21:24:24 -0500

    fixed router export

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -27,3 +27,6 @@ router.param('userId', userCtrl.userByID)
 router.route('/api/users/:userId').get(userCtrl.read)
 router.route('/api/users/:userId').put(userCtrl.update)
 router.route('/api/users/:userId').delete(userCtrl.remove)
+
+
+export default router
