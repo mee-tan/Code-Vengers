7fadedb10b0b99613c981fb1d524ed2441d96e68
Lucianna Mendonca <luciannamend@gmail.com>
2023-12-13 18:59:16 -0500

    fixed authentication

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,57 +1,60 @@
+
 /**
-* Name: group 3 
-* Date: 17th November 2023
-* Description: controll authorization for users 
+ * Name: group 3
+ * Date: 17th November 2023
+ * Description: controll authorization for users
  */
 
-import User from '../models/user.model.js'
-import jwt from 'jsonwebtoken'
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 import { expressjwt } from "express-jwt";
-import config from './../../config/config.js'
+import config from "./../../config/config.js";
 
-
-const signin = async (req, res) => { 
-try {
-let user = await User.findOne({ "email": req.body.email }) 
-if (!user)
-return res.status('401').json({ error: "User not found" }) 
-if (!user.authenticate(req.body.password)) {
-return res.status('401').send({ error: "Email and password don't match." })
-}
-const token = jwt.sign({ _id: user._id }, config.jwtSecret) 
-res.cookie('t', token, { expire: new Date() + 9999 }) 
-return res.json({
-token, 
-user: {
-_id: user._id, 
-name: user.name,
-email: user.email 
-}
-})
-} catch (err) {
-return res.status('401').json({ error: "Could not sign in" }) 
-}
-}
+const signin = async (req, res) => {
+  try {
+    let user = await User.findOne({ "email": req.body.email });
+    if (!user) return res.status("401").json({ error: "User not found" });
+    if (!user.authenticate(req.body.password)) {
+      return res
+        .status("401")
+        .send({ error: "Email and password don't match." });
+    }
+    const token = jwt.sign({ _id: user._id }, config.jwtSecret); 
+    res.cookie("t", token, { expire: new Date() + 9999 });
+    return res.json({
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (err) {
+    return res.status("401").json({ error: "Could not sign in" });
+  }
+};
 
 const signout = (req, res) => { 
-res.clearCookie("t")
-return res.status('200').json({ 
-message: "signed out"
-}) 
-}
+  res.clearCookie("t");
+  return res.status("200").json({
+    message: "Signed out",
+  });
+};
+
 const requireSignin = expressjwt({ 
-    secret: config.jwtSecret, 
-    algorithms: ["HS256"],
-userProperty: 'auth'
-})
+  secret: config.jwtSecret,
+  algorithms: ["HS256"],
+  userProperty: "auth",
+});
+
 const hasAuthorization = (req, res, next) => { 
-const authorized = req.profile && req.auth
-&& req.profile._id ==  req.auth._id 
-if (!(authorized)) {
-return res.status('403').json({ 
-error: "User is not authorized"
-}) 
-} 
-next()
-}
-export default { signin, signout, requireSignin, hasAuthorization }
+  const authorized = req.profile && req.auth && req.profile._id == req.auth._id; 
+  if (!authorized) {
+    return res.status("403").json({
+      error: "User is not authorized",
+    });
+  }
+  next();
+};
+
+export default { signin, signout, requireSignin, hasAuthorization };
--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,75 +1,81 @@
 /**
-* Name: group 3 
-* Date: 17th November 2023
-* Description: model for users 
+ * Name: group 3
+ * Date: 17th November 2023
+ * Description: model for users
  */
-import mongoose from 'mongoose'
-import crypto from 'crypto';
+import mongoose from "mongoose";
+import crypto from "crypto";
+
 const UserSchema = new mongoose.Schema({
- name: {
- type: String,
- trim: true,
- required: 'Name is required'
- },
- email: {
- type: String,
- trim: true,
-unique: 'Email Address already exists',
-match: [/.+\@.+\..+/, 'Please enter a valid email address'],
-required: 'Email Address is required'
-},
- created: {
-type: Date,
-default: Date.now
-},
- updated: {
-type: Date,
-default: Date.now
-},
-hashed_password: {
-type: String,
-required: 'Password is required'
-},
-salt: String
+  //this properties will be stored in the database
+  name: {
+    type: String,
+    trim: true,
+    required: "Name is required",
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: "Email Address already exists",
+    match: [/.+\@.+\..+/, "Please enter a valid email address"],
+    required: "Email Address is required",
+  },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+  updated: {
+    type: Date,
+    default: Date.now,
+  },
+  hashed_password: {
+    type: String,
+    required: "Password is required",
+  },
+  salt: String,
 });
-UserSchema.virtual('password')
- .set(function(password) {
- this._password = password;
- this.salt = this.makeSalt(); 
- this.hashed_password = this.encryptPassword(password)
- //this.hashed_password = password;
-})
-.get(function() {
-return this._password;
- });
-UserSchema.path('hashed_password').validate(function(v) {
- if (this._password && this._password.length < 6) {
- this.invalidate('password', 'Password must be at least 6 characters.');
-}
- if (this.isNew && !this._password) {
-this.invalidate('password', 'Password is required');
- }
+
+UserSchema.virtual("password") 
+  .set(function (password) {
+    this._password = password;
+    this.salt = this.makeSalt(); 
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function () {
+    return this._password;
+  });
+  
+UserSchema.path("hashed_password").validate(function (v) { 
+  if (this._password && this._password.length < 6) {
+    this.invalidate("password", "Password must be at least 6 characters.");
+  }
+  if (this.isNew && !this._password) {
+    this.invalidate("password", "Password is required");
+  }
 }, null);
+
 UserSchema.methods = {
-authenticate: function(plainText) {
-  console.log('encrypted',this.encryptPassword(plainText), 
-  'hashed', this.hashed_password)
-return plainText === this.hashed_password 
-},
-encryptPassword: function(password) { 
-if (!password) return ''
-try {
-return crypto
-.createHmac('sha1', this.makeSalt()) 
-.update(password)
-.digest('hex') 
-} catch (err) {
-    console.log(err);
-return '' 
-}
-},
-makeSalt: function() {
-return Math.round((new Date().valueOf() * Math.random())) + '' 
-}
-}
-export default mongoose.model('User', UserSchema);
+
+  authenticate: function (plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password; 
+  },
+
+  encryptPassword: function (password) { 
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt) 
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      console.log(err);
+      return "";
+    }
+  },
+
+  makeSalt: function () {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  },
+};
+
+export default mongoose.model("User", UserSchema);
